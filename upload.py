f53e5f1b5a9858934f6cf38bcd9f2e9bb8b82bb0
Taeuk Noh <taeuk4958@gmail.com>
2022-08-19 11:00:41 +0900

    for test

--- upload.py
+++ upload.py
@@ -1,9 +1,4 @@
-import subprocess
-import os
+# dvc status
 
-secret_file_path = os.path.join(os.getcwd(), "adroit-producer-358501-0b55d0921ca3.json")
-set_ = f"export GOOGLE_APPLICATION_CREDENTIALS='D:\Noh_TaeUk\pipeline_project\pipeline_dataset\adroit-producer-358501-0b55d0921ca3.json'"
-print(f"set_ : {set_}")
-exit()
-set_2 = f"dvc remote modify --local bikes credentialpath '{secret_file_path}'"
-subprocess.call([set_2], shell = True)
+# export GOOGLE_APPLICATION_CREDENTIALS=client_secrets.json
+# dvc push
