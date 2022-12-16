db133f74474d8035bd2748b9fcdccda09d377c6f
Taeuk Noh <taeuk4958@gmail.com>
2022-12-16 20:37:24 +0900

    create_table import from cusom package

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,25 +1,10 @@
-
-
-def create_table(cursor, table_name, schema):
-    cursor.execute(f"SHOW TABLES")
-    fetchs = cursor.fetchall()
-    if len(fetchs) !=0:
-        tables = fetchs[0]
-        if table_name not in tables:
-            print(f"  mysql>> create table: {table_name}")
-            cursor.execute(schema)
-        else:
-            print(f"  mysql>> table: {table_name} is already exist!")
-    else:
-        print(f"  mysql>> create table: {table_name}")
-        cursor.execute(schema)
-        
+from hibernation_no1.database.mysql import create_table
         
 def whether_run_commit(cfg, database, image_list):
     print(f" >> confirm before commit to database: {cfg.db.db_name}")
     print(f" >>     number of images: {len(image_list)}")
     print(f" >>     category of dataset: {cfg.dvc.dataset_cate}")
-    print(f" >>     version of annotations dataset: {cfg.dvc.ann_version}")
+    print(f" >>     version of annotations dataset: {cfg.dvc.ann.version}")
     print(f" >>\n           enter 'y' to commit ")
     commit = input()
     
