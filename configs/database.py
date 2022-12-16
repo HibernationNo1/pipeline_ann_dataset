a09d0f4979fe27a33fd0295b5cc8a39cd24ecdc9
Taeuk Noh <taeuk4958@gmail.com>
2022-12-16 11:32:31 +0900

    fix typos

--- configs/database.py
+++ configs/database.py
@@ -28,6 +28,7 @@ db = dict(
                                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                                       dataset_purpose VARCHAR(10) NOT NULL,
                                       image_name VARCHAR(200) NOT NULL,
+                                      recode_file VARCHAR(200) NOT NULL, 
                                       category VARCHAR(20) NOT NULL,
                                       recode_version VARCHAR(10) NOT NULL,
                                       PRIMARY KEY(id)
@@ -44,13 +45,14 @@ db = dict(
                                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                                       dataset_purpose VARCHAR(10) NOT NULL,
                                       category VARCHAR(20) NOT NULL,
-                                      dataset_name VARCHAR(200) NOT NULL,
+                                      recode_file VARCHAR(200) NOT NULL,
                                       recode_version VARCHAR(10) NOT NULL,
                                       PRIMARY KEY(id)
                                       );
                                       """
             # dataset_purpose: one of `train` and `val`
-            # dataset_name: path of train_dataset or val_dataset file for train or validation
+            # recode_file: path of recode dataset file(.json format). 
+            #              are for train or validation(e.g. train_dataset.json or val_data.json)
             # category: category of dataset
             # recode_version: version of recoded dataset 
     )
--- configs/dvc.py
+++ configs/dvc.py
@@ -4,5 +4,8 @@ dvc = dict(
     dataset_cate = "test_dataset",
     # test_dataset, proejct_dataset
     
-    ann_version = "0.0.1"
+    ann_version = "0.0.1",
+
+    ann_dataset_bucket_name = "ann_dataset_taeuk4958",
+    record_dataset_bucket_name = 'record_dataset_taeuk4958'
     )
