2db9579ab3abd54764002418f02111c4bffe27d6
Taeuk Noh <taeuk4958@gmail.com>
2023-01-06 09:18:32 +0900

    test_dataset:: ann:: 0.0.3

--- dvs.sh
+++ dvs.sh
@@ -1,4 +1,4 @@
-# dvc add test_dataset/ann/0.0.1
-# git add test_dataset/ann/0.0.1.dvc  test_dataset/ann/.gitignore
-# git commit -m "test_dataset:: ann:: 0.0.1"
+# dvc add test_dataset/ann/0.0.3
+# git add test_dataset/ann/0.0.3.dvc  test_dataset/ann/.gitignore
+# git commit -m "test_dataset:: ann:: 0.0.3"
 # dvc push
