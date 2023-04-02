992d162fa8c41551015db1e4ac0ae121c2eb38c9
HibernationNo1 <taeuk4958@gmail.com>
2023-04-02 14:52:58 +0900

    add if Config().dvc_dir is None

--- utils/dvc_utils.py
+++ utils/dvc_utils.py
@@ -3,8 +3,14 @@ import glob
 import re
 from dvc.config import Config
 
-def get_dvc_config(remote_user_config):
-    dvc_config_path = osp.join(Config().dvc_dir, "config")
+def check_dvc_config(remote_user_config):
+    if Config().dvc_dir is None:
+        dvc_config_path = osp.join(os.getcwd(), ".dvc", "config")
+    else:
+        dvc_config_path = osp.join(Config().dvc_dir, "config")
+    
+    assert osp.isfile(dvc_config_path), f" Config file of dvc is not exist!  Check path: {dvc_config_path}"
+ 
     with open(dvc_config_path, "r") as dvc_config:
         # in here, will be erased all contents of 'dvc_config'
         assert len(list(dvc_config)) > 2,  "No remote configuration!  run 'remote add'!"
@@ -47,8 +53,7 @@ def get_dvc_config(remote_user_config):
     
     for remote, url in zip(remotes, urls):
         dvc_cfg['remotes'].append(dict(remote = remote, url = url))
-        
-        
+    
     if dvc_cfg['defualt_remote'] != remote_user_config: 
         raise OSError(f"Defualt_remote '.dvc/config: {dvc_cfg['defualt_remote']}' and "\
                       f"'cfg: {remote_user_config}' are not same!!")
@@ -56,23 +61,38 @@ def get_dvc_config(remote_user_config):
     return dvc_cfg
 
 
+def check_dvc_credential(dvc_cfg):
+    if Config().dvc_dir is None:
+        credentials = osp.join(os.getcwd(), ".dvc", "config.local")
+    else:
+        credentials = osp.join(Config().dvc_dir, "config.local")
+        
+    assert osp.isfile(credentials), f"\n  >> Path: {credentials} is not exist!!   "\
+        f"set google storage credentials! "\
+        f"\n  >> run:   $ dvc remote modify --local {dvc_cfg['defualt_remote']} credentialpath `client_secrets_path`"
+            
+            
+            
+
 def check_dvc_dataset_status(cfg):
-    target_dataset = osp.join(os.getcwd(), cfg.dvc.dataset_cate,
-                                           cfg.dvc.ann.name,
-                                           cfg.dvc.ann.version)
+    target_dataset = osp.join(os.getcwd(), cfg.dvc.target_dataset.name,
+                                           cfg.dvc.target_dataset.category)
     assert osp.isdir(target_dataset), f"\n>> Path: {target_dataset} is not exist!!"  
-    
+ 
     # comfirm dataset quantity
     image_list = glob.glob(target_dataset +"/*.jpg")
     json_list = glob.glob(target_dataset +"/*.json")
     assert len(image_list) == len(json_list), f"number of images and json files are not same!!  \n"\
         f"number of images {len(image_list)},     number of json files : {len(json_list)}"\
     
-    dvc_cfg = get_dvc_config(cfg.dvc.remote)
+    # Check dvc config setting
+    dvc_cfg = check_dvc_config(cfg.dvc.target_dataset.name)
     
-    credentials = osp.join(os.getcwd(), ".dvc", "config.local")
-    assert osp.isfile(credentials), f"\n  >> Path: {credentials} is not exist!!   "\
-        f"set google storage credentials! "\
-        f"\n  >> run:   $ dvc remote modify --local {dvc_cfg['defualt_remote']} credentialpath `client_secrets_path`"
-                                    
-    return image_list, json_list, target_dataset
+    # Check google-dvc credential 
+    check_dvc_credential(dvc_cfg)
+    
+    target_dataset_dvc = osp.join(os.getcwd(), f"{target_dataset}.dvc")
+    assert osp.isfile(target_dataset_dvc), f"\n>> Path: {target_dataset_dvc} is not exist!!"\
+            f"\n>> run      $ dvc add {osp.basename(target_dataset_dvc).split('.')[0]}"
+    
+    return image_list, json_list
