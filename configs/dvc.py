b9eff3ba1285227f5a73326f8527dd094bc81f90
hibernation <taeuk4958@gmail.com>
2023-04-06 17:24:09 +0900

    test: verion update, now branch

--- configs/dvc.py
+++ configs/dvc.py
@@ -2,7 +2,7 @@ dvc = dict(
     target_dataset = dict(
         name = 'ann_dataset',		 # name of remote
         category = "board_dataset",   # test_dataset
-        version = '0.0.1'
+        version = '0.0.2'
     ),
     
     
